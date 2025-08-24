0e73c1d7cae8995e2cd76329a90f03594b07ffe3
Balbino Pedro Baptista <dinubatista4@gmail.com>
2025-08-25 00:36:59 +0700

    debug error on railway

--- database/db_connection/connect.go
+++ database/db_connection/connect.go
@@ -18,16 +18,19 @@ func OpenDB() (*sql.DB, error) {
 	}
 
 	log.Println("Connecting to database...")
-	db, err := sql.Open("postgres", url)
+	DB, err := sql.Open("postgres", url)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open database: %w", err)
 	}
 
-	if err := db.Ping(); err != nil {
+	if err := DB.Ping(); err != nil {
 		return nil, fmt.Errorf("failed to ping database: %w", err)
 	}
 
 	log.Println("Database ping successful")
-	DB = db
+
 	return DB, nil
+
+
+	
 }
